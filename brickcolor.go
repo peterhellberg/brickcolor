1c2ed556d645dc87a9e748f3515b39f205b4f998
Peter Hellberg <peter@c7.se>
2018-12-09 22:15:32 +0100

    Initial package comment

--- brickcolor.go
+++ brickcolor.go
@@ -1,11 +1,21 @@
+/*
+
+	Package brickcolor includes all of the Roblox BrickColor Codes as
+	listed on https://developer.roblox.com/articles/BrickColor-Codes
+
+	Installation
+
+			go get -u github.com/peterhellberg/brickcolor
+
+*/
 package brickcolor
+
+//go:generate go run -tags=generator _generator/main.go
 
 import (
 	"image/color"
 	"math/rand"
 )
-
-//go:generate go run -tags=generator _generator/main.go
 
 // BrickColor contains the color, name, number and hex value for a Roblox part color
 type BrickColor struct {
